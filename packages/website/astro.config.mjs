8d228465a6ed0748f6cb88d9d525493b7031d35c
xHyroM <generalkubo@gmail.com>
2023-05-09 20:43:18 +0200

    fix: seo & accessibility

--- packages/website/astro.config.mjs
+++ packages/website/astro.config.mjs
@@ -4,6 +4,9 @@ import { CONFIG } from "./src/config";
 import compress from "astro-compress";
 import tailwind from "@astrojs/tailwind";
 
+import sitemap from "@astrojs/sitemap";
+import robotsTxt from "astro-robots-txt";
+
 import path from "path";
 import { fileURLToPath } from "url";
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
@@ -13,13 +16,23 @@ export default defineConfig({
   site: CONFIG.origin,
   base: "/",
   integrations: [
+    sitemap(),
+    robotsTxt({
+      policy: [
+        {
+          userAgent: "*",
+        },
+      ],
+      sitemap: true,
+    }),
     compress({
       css: true,
       html: true,
       img: true,
       js: true,
-      svg: true
-    }), tailwind(),
+      svg: true,
+    }),
+    tailwind(),
   ],
   vite: {
     resolve: {
